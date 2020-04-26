496581b9cdebcd292b6a5a2caa81e7d7653f373c
Raphael Vidinha <raphael.vidinha@gmail.com>
2020-04-26 10:50:53 -0300

    refatoração do titlesHelper

--- helpers/titlesHelper.js
+++ helpers/titlesHelper.js
@@ -1,8 +1,9 @@
 module.exports = {
   removeUnwantedPhrases: (titles, unwantedPhrases) => {
-    for(let i = 0; i <= (unwantedPhrases.length - 1); i++) {
-      titles = titles.toString().replace(unwantedPhrases[i].toString(), '');
-    }
+    unwantedPhrases.forEach(phrase => {
+      titles = titles.toString().replace(phrase.toString(), '');
+    });
+    
     return titles;
   }
 }
